7464c8bb78e00c2942d9dd146f20cee4eaebbfce
Ldfm <59668035+Leon-maker@users.noreply.github.com>
2024-09-27 13:33:40 +0200

    menu

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -106,65 +106,67 @@ const Main = () => {
                             </div>
                         </div>
 
-                        {searchResults.length > 0 && (
-
-                            <div className="card-container">
-                                {searchResults.map((item, index) => (
-                                    <a
-                                        key={index}
-                                        className="card-result"
-                                        href={item.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <h2>{item.title}</h2>
-                                        <p>{item.description.substring(0, 100)}...</p>
-                                        <div className="btn-discover">
-                                            <span>Découvrir l&apos;article</span>
+                        <div className="main-welcome">
+                            <div className="main-top">
+                                <form className="search-box" onSubmit={handleSearch}>
+                                    <input
+                                        type="text"
+                                        placeholder="Entrer votre recherche"
+                                        value={searchTerm}
+                                        onChange={(e) => setSearchTerm(e.target.value)}
+                                    />
+                                    <button type="submit">
+                                        <Image src={assets.send_icon} alt="Envoyer" />
+                                    </button>
+                                </form>
+                            </div>
+                            {searchResults.length === 0 && (
+                                <div className="welcome-content">
+                                    <div className="greet">
+                                        <p><span>Bonjour,</span></p>
+                                        <p>Que puis-je faire pour vous ?</p>
+                                    </div>
+                                    <div className="cards">
+                                        <div className="card">
+                                            <p>Quelle différence entre mandat et procuration ?</p>
+                                            <Image src={assets.compass_icon} alt="" />
                                         </div>
-                                    </a>
-                                ))}
-                            </div>
-                        )}
-                        {searchResults.length === 0 && (
-                            <div className="main-welcome">
-                                <div className="main-top">
-                                    <form className="search-box" onSubmit={handleSearch}>
-                                        <input
-                                            type="text"
-                                            placeholder="Entrer votre recherche"
-                                            value={searchTerm}
-                                            onChange={(e) => setSearchTerm(e.target.value)}
-                                        />
-                                        <button type="submit">
-                                            <Image src={assets.send_icon} alt="Envoyer" />
-                                        </button>
-                                    </form>
-                                </div>
-                                <div className="greet">
-                                    <p><span>Bonjour,</span></p>
-                                    <p>Que puis-je faire pour vous ?</p>
-                                </div>
-                                <div className="cards">
-                                    <div className="card">
-                                        <p>Quelle différence entre mandat et procuration ?</p>
-                                        <Image src={assets.compass_icon} alt="" />
-                                    </div>
-                                    <div className="card">
-                                        <p>Quand invoquer la force majeure ?</p>
-                                        <Image src={assets.bulb_icon} alt="" />
-                                    </div>
-                                    <div className="card">
-                                        <p>Différence entre responsabilité civile et pénale ?</p>
-                                        <Image src={assets.message_icon} alt="" />
-                                    </div>
-                                    <div className="card">
-                                        <p>Quels droits pour un salarié licencié ?</p>
-                                        <Image src={assets.code_icon} alt="" />
+                                        <div className="card">
+                                            <p>Quand invoquer la force majeure ?</p>
+                                            <Image src={assets.bulb_icon} alt="" />
+                                        </div>
+                                        <div className="card">
+                                            <p>Différence entre responsabilité civile et pénale ?</p>
+                                            <Image src={assets.message_icon} alt="" />
+                                        </div>
+                                        <div className="card">
+                                            <p>Quels droits pour un salarié licencié ?</p>
+                                            <Image src={assets.code_icon} alt="" />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )}
+                            )}
+                            {searchResults.length > 0 && (
+
+                                <div className="card-container">
+                                    {searchResults.map((item, index) => (
+                                        <a
+                                            key={index}
+                                            className="card-result"
+                                            href={item.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <h2>{item.title}</h2>
+                                            <p>{item.description.substring(0, 100)}...</p>
+                                            <div className="btn-discover">
+                                                <span>Découvrir l&apos;article</span>
+                                            </div>
+                                        </a>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
